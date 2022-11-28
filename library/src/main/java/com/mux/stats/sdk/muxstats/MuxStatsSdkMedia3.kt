0c3e2131374d4714af617f304edfcefd2038d6f2
Emily Dixon <edixon@mux.com>
2022-11-28 14:40:21 -0800

    SDK facade that extends MuxDataSDK

--- library/src/main/java/com/mux/stats/sdk/muxstats/MuxStatsSdkMedia3.kt
+++ library/src/main/java/com/mux/stats/sdk/muxstats/MuxStatsSdkMedia3.kt
@@ -1,4 +1,35 @@
 package com.mux.stats.sdk.muxstats
 
-class MuxStatsSdkMedia3 {
+import android.content.Context
+import android.view.View
+import androidx.media3.common.Player
+import com.mux.stats.sdk.core.CustomOptions
+import com.mux.stats.sdk.core.model.CustomerData
+import com.mux.stats.sdk.muxstats.internal.Media3PlayerBinding
+import com.mux.stats.sdk.muxstats.media3.BuildConfig
+
+class MuxStatsSdkMedia3(
+  context: Context,
+  envKey: String,
+  customerData: CustomerData,
+  player: Player,
+  playerView: View? = null,
+  customOptions: CustomOptions? = null,
+) : MuxDataSdk<Player, View>(
+  context= context,
+  envKey = envKey,
+  player = player,
+  playerView = playerView,
+  customerData = customerData,
+  customOptions = customOptions ?: CustomOptions(),
+  trackFirstFrame = true,
+  playerBinding = Media3PlayerBinding(),
+  device = AndroidDevice(
+    ctx = context,
+    playerVersion = BuildConfig.MEDIA3_VERSION, /* TODO: Dynamic would be better if possible*/
+    muxPluginName = "mux-media3" ,
+    muxPluginVersion = BuildConfig.LIB_VERSION,
+    playerSoftware = "media3",
+  )
+) {
 }
---
+++ library/src/main/java/com/mux/stats/sdk/muxstats/internal/Media3PlayerBinding.kt
@@ -0,0 +1,19 @@
+package com.mux.stats.sdk.muxstats.internal
+
+import androidx.media3.common.Player
+import com.mux.stats.sdk.muxstats.MuxPlayerAdapter
+import com.mux.stats.sdk.muxstats.MuxStateCollector
+
+/**
+ *
+ */
+internal class Media3PlayerBinding : MuxPlayerAdapter.PlayerBinding<Player> {
+  override fun bindPlayer(player: Player, collector: MuxStateCollector) {
+    TODO("Not yet implemented")
+  }
+
+  override fun unbindPlayer(player: Player, collector: MuxStateCollector) {
+    TODO("Not yet implemented")
+  }
+
+}
