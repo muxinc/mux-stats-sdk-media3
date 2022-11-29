43932171786158bb0cbde052a45ba495068e9998
Emily Dixon <edixon@mux.com>
2022-11-29 14:34:28 -0800

    Add Initial Media3 Implementation (#1)

---
+++ library/src/main/java/com/mux/stats/sdk/muxstats/internal/Media3PlayerBinding.kt
@@ -0,0 +1,64 @@
+package com.mux.stats.sdk.muxstats.internal
+
+import androidx.media3.common.Player
+import androidx.media3.common.Timeline
+import androidx.media3.common.Tracks
+import com.mux.stats.sdk.muxstats.MuxPlayerAdapter
+import com.mux.stats.sdk.muxstats.MuxStateCollector
+import java.lang.ref.WeakReference
+
+/**
+ * Creates a new instance of the generic Media3 PlayerBinding. Will work with any [Player],
+ * including a MediaController
+ */
+internal fun media3GenericBinding(): MuxPlayerAdapter.PlayerBinding<Player> = Media3PlayerBinding()
+
+/**
+ * PlayerBinding for a generic Media3 [Player]
+ */
+private class Media3PlayerBinding : MuxPlayerAdapter.PlayerBinding<Player> {
+
+  private var listener: MuxPlayerListener? = null
+
+  override fun bindPlayer(player: Player, collector: MuxStateCollector) {
+    listener = MuxPlayerListener(player, collector).also { player.addListener(it) }
+  }
+
+  override fun unbindPlayer(player: Player, collector: MuxStateCollector) {
+    listener?.let { player.removeListener(it) }
+    collector.playerWatcher?.stop("player unbound")
+    listener = null
+  }
+
+}
+
+private class MuxPlayerListener(player: Player, val collector: MuxStateCollector) :
+  Player.Listener {
+  val player by weak(player)
+
+  override fun onPlaybackStateChanged(playbackState: Int) {
+    // We rely on the player's playWhenReady because the order of this callback and its callback
+    //  is not well-defined
+    player?.let { collector.handleExoPlaybackState(playbackState, it.playWhenReady) }
+  }
+
+  override fun onPositionDiscontinuity(
+    oldPosition: Player.PositionInfo,
+    newPosition: Player.PositionInfo,
+    reason: Int
+  ) {
+    collector.handlePositionDiscontinuity(reason)
+  }
+
+  override fun onTimelineChanged(timeline: Timeline, reason: Int) {
+    timeline.takeIf { it.windowCount > 0 }?.let { tl ->
+      val window = Timeline.Window().apply { tl.getWindow(0, this) }
+      collector.sourceDurationMs = window.durationMs
+    }
+  }
+
+  override fun onTracksChanged(tracks: Tracks) {
+    watchPlayerPos(WeakReference(player), collector)
+    collector.mediaHasVideoTrack = tracks.hasAtLeastOneVideoTrack()
+  }
+}
---
+++ library/src/main/java/com/mux/stats/sdk/muxstats/internal/Utils.kt
@@ -0,0 +1,112 @@
+package com.mux.stats.sdk.muxstats.internal
+
+import androidx.media3.common.Player
+import androidx.media3.common.Tracks
+import com.mux.stats.sdk.muxstats.MuxPlayerState
+import com.mux.stats.sdk.muxstats.MuxStateCollector
+import java.lang.ref.WeakReference
+
+// -- General Utils --
+
+internal const val PLAYER_STATE_POLL_MS = 150L
+
+/**
+ * Returns true if the object is one of any of the parameters supplied
+ */
+@JvmSynthetic // Hide from Java callers because all are external
+internal fun Any.oneOf(vararg accept: Any) = accept.contains(this)
+
+/**
+ * Returns true if the object is not any of the parameters supplied
+ */
+@JvmSynthetic // Hide from Java callers because all are external
+internal fun Any.noneOf(vararg accept: Any) = !accept.contains(this)
+
+/**
+ * Gets a Log Tag from the name of the calling class. Can be used in any package that isn't
+ * obfuscated (such as muxstats)
+ */
+@Suppress("unused")
+internal inline fun <reified T> T.logTag() = T::class.java.simpleName
+
+internal fun watchPlayerPos(player: WeakReference<Player>, collector: MuxStateCollector) {
+  collector.playerWatcher = MuxStateCollector.PlayerWatcher(
+    PLAYER_STATE_POLL_MS,
+    collector,
+    player
+  ) { it, _ -> it.currentPosition }
+  collector.playerWatcher?.start()
+}
+
+// -- Media3 Utils
+
+/**
+ * Returns true if any media track in the given [Tracks] object had a video MIME type
+ */
+internal fun Tracks.hasAtLeastOneVideoTrack(): Boolean {
+  return groups.map { it.mediaTrackGroup }
+    .filter { trackGroup -> trackGroup.length > 0 }
+    .map { trackGroup -> trackGroup.getFormat(0) }
+    .find { format -> format.sampleMimeType?.contains("video") ?: false }
+    .let { foundVideoTrack -> foundVideoTrack != null }
+}
+
+/**
+ * Handles an ExoPlayer position discontinuity
+ */
+@JvmSynthetic // Hides from java
+internal fun MuxStateCollector.handlePositionDiscontinuity(reason: Int) {
+  when (reason) {
+    Player.DISCONTINUITY_REASON_SEEK -> {
+      // If they seek while paused, this is how we know the seek is complete
+      if (muxPlayerState == MuxPlayerState.PAUSED
+        // Seeks on audio-only media are reported this way instead
+        || !mediaHasVideoTrack!!
+      ) {
+        seeked(false)
+      }
+    }
+    else -> {} // ignored
+  }
+}
+
+/**
+ * Handles a change of basic ExoPlayer state
+ */
+@JvmSynthetic // Hidden from Java callers, since the only ones are external
+internal fun MuxStateCollector.handleExoPlaybackState(
+  playbackState: Int, // the @IntDef for player state omitted. Unavailable on all exo versions
+  playWhenReady: Boolean
+) {
+  if (this.muxPlayerState == MuxPlayerState.PLAYING_ADS) {
+    // Normal playback events are ignored during ad playback
+    return
+  }
+
+  when (playbackState) {
+    Player.STATE_BUFFERING -> {
+      buffering()
+      if (playWhenReady) {
+        play()
+      } else if (muxPlayerState != MuxPlayerState.PAUSED) {
+        pause()
+      }
+    }
+    Player.STATE_READY -> {
+      if (playWhenReady) {
+        playing()
+      } else if (muxPlayerState != MuxPlayerState.PAUSED) {
+        pause()
+      }
+    }
+    Player.STATE_ENDED -> {
+      ended()
+    }
+    Player.STATE_IDLE -> {
+      if (muxPlayerState.oneOf(MuxPlayerState.PLAY, MuxPlayerState.PLAYING)) {
+        // If we are playing/preparing to play and go idle, the player was stopped
+        pause()
+      }
+    }
+  } // when (playbackState)
+} // fun handleExoPlaybackState
