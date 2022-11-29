e7f3d28e62dff3a39334f503be211b09b512ed28
Emily Dixon <edixon@mux.com>
2022-11-29 14:50:27 -0800

    Add some javadoc to the facade

--- library/src/main/java/com/mux/stats/sdk/muxstats/Player.kt
+++ library/src/main/java/com/mux/stats/sdk/muxstats/Player.kt
@@ -5,8 +5,6 @@ import android.view.View
 import androidx.media3.common.Player
 import com.mux.stats.sdk.core.CustomOptions
 import com.mux.stats.sdk.core.model.CustomerData
-import com.mux.stats.sdk.muxstats.internal.media3GenericBinding
-import com.mux.stats.sdk.muxstats.media3.BuildConfig
 
 /**
  * Monitor this Player with Mux Data, reporting data about the View to the environment specified
@@ -15,31 +13,21 @@ import com.mux.stats.sdk.muxstats.media3.BuildConfig
  * @param context a Context containing your player. If it's an Activity, screen size is obtained
  * @param envKey your Mux Data Environment Key
  * @param customerData data about you, your customer, and your video
- * @param player the player you wish to observe
  * @param playerView the View showing your video content
  * @param customOptions Options that affect the behavior of the SDK
  */
-class MuxStatsSdkMedia3(
+fun Player.monitorWithMuxData(
   context: Context,
   envKey: String,
   customerData: CustomerData,
-  player: Player,
   playerView: View? = null,
-  customOptions: CustomOptions? = null,
-) : MuxDataSdk<Player, View>(
-  context= context,
+  customOptions: CustomOptions? = null
+): MuxDataSdk<Player, View> = MuxStatsSdkMedia3(
+  context = context,
   envKey = envKey,
-  player = player,
-  playerView = playerView,
   customerData = customerData,
-  customOptions = customOptions ?: CustomOptions(),
-  trackFirstFrame = true,
-  playerBinding = media3GenericBinding(),
-  device = AndroidDevice(
-    ctx = context,
-    playerVersion = BuildConfig.MEDIA3_VERSION, /* TODO: Dynamic would be better if possible*/
-    muxPluginName = "mux-media3" ,
-    muxPluginVersion = BuildConfig.LIB_VERSION,
-    playerSoftware = "media3",
-  )
+  player = this,
+  playerView = playerView,
+  customOptions = customOptions
 )
+
