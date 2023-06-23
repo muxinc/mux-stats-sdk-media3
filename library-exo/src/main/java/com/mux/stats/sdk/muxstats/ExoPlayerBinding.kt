d5ac8b3d1d85778f98fbed0c7d544a17f3eca3a9
Emily Dixon <edixon@mux.com>
2023-06-23 12:50:12 -0700

    Start on the player binding

--- library-exo/src/main/java/com/mux/stats/sdk/muxstats/ExoPlayerBinding.kt
+++ library-exo/src/main/java/com/mux/stats/sdk/muxstats/ExoPlayerBinding.kt
@@ -1,13 +1,26 @@
 package com.mux.stats.sdk.muxstats
 
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.analytics.AnalyticsListener
+import com.mux.android.util.weak
 
 class ExoPlayerBinding : MuxPlayerAdapter.PlayerBinding<ExoPlayer> {
+
+  private var listener: MuxAnalyticsListener? = null
+
   override fun bindPlayer(player: ExoPlayer, collector: MuxStateCollector) {
-    TODO("Not yet implemented")
+    listener = MuxAnalyticsListener(player, collector).also { player.addAnalyticsListener(it) }
   }
 
   override fun unbindPlayer(player: ExoPlayer, collector: MuxStateCollector) {
-    TODO("Not yet implemented")
+    listener?.let { player.removeAnalyticsListener(it) }
+    collector.playerWatcher?.stop("player unbound")
+    listener = null
   }
+}
+
+private class MuxAnalyticsListener(player: ExoPlayer, val collector: MuxStateCollector)
+  : AnalyticsListener {
+
+    private val player by weak(player)
 }
