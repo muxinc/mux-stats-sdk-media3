24d2bd082812c36792061353c8f776d59eeb6950
Emily Dixon <edixon@mux.com>
2023-08-08 17:04:58 -0700

    maint: Add support for media3 v1.1.x (#26) (#27)
    # Updates
    
    * maint: Add support for media3 v1.1.x (#26)

--- app/src/main/java/com/mux/stats/muxdatasdkformedia3/Util.kt
+++ app/src/main/java/com/mux/stats/muxdatasdkformedia3/Util.kt
@@ -3,5 +3,5 @@ package com.mux.stats.muxdatasdkformedia3
 import android.net.Uri
 import androidx.media3.common.MediaItem
 
-internal fun String.toUri() = Uri.parse(this)
-internal fun String.toMediaItem() = MediaItem.fromUri(toUri())
+fun String.toUri(): Uri = Uri.parse(this)
+fun String.toMediaItem() = MediaItem.fromUri(toUri())
