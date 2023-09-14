a98dbb14ab2fc05e64d3867fcdb808e4eb2c1a99
Emily Dixon <edixon@mux.com>
2023-09-14 15:20:52 -0700

    Revert "Add Live Playback example"
    This reverts commit 6c5dc87bf6e2cdd6fa47e5b6ad7b93e9e51de9c1.

--- app/src/main/java/com/mux/stats/muxdatasdkformedia3/Constants.kt
+++ app/src/main/java/com/mux/stats/muxdatasdkformedia3/Constants.kt
@@ -8,5 +8,4 @@ object Constants {
   const val VOD_TEST_URL_BIG_BUCK_BUNNY = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
   const val AD_TAG_SIMPLE_W_MID_ROLL = "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpremidpostoptimizedpod&ciu_szs=300x250&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&cmsid=496&vid=short_onecue&correlator="
   const val AD_TAG_COMPLEX = "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpremidpostlongpod&ciu_szs=300x250&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&cmsid=496&vid=short_onecue&correlator="
-  const val LIVE_TEST_BIG_BUCK_BUNNY = "https://stream.mux.com/v69RSHhFelSm4701snP22dYz2jICy4E4FUyk02rW4gxRM.m3u8"
 }
