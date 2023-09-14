fafc42b5f07eb84e13192280010df8de81773318
Emily Dixon <edixon@mux.com>
2023-09-14 11:28:14 -0700

    Test errors too

--- library-ima/src/test/java/com/mux/stats/sdk/media3_ima/MuxImAdsListenerTests.kt
+++ library-ima/src/test/java/com/mux/stats/sdk/media3_ima/MuxImAdsListenerTests.kt
@@ -2,6 +2,11 @@ package com.mux.stats.sdk.media3_ima
 
 import androidx.media3.common.Player
 import com.google.ads.interactivemedia.v3.api.Ad
+import com.google.ads.interactivemedia.v3.api.AdError
+import com.google.ads.interactivemedia.v3.api.AdError.AdErrorCode
+import com.google.ads.interactivemedia.v3.api.AdError.AdErrorType
+import com.google.ads.interactivemedia.v3.api.AdErrorEvent
+import com.google.ads.interactivemedia.v3.api.AdErrorEvent.AdErrorListener
 import com.google.ads.interactivemedia.v3.api.AdEvent
 import com.google.ads.interactivemedia.v3.api.AdEvent.AdEventListener
 import com.mux.stats.sdk.muxstats.MuxStatsSdkMedia3
@@ -55,6 +60,57 @@ class MuxImAdsListenerTests : AbsRobolectricTest() {
         override fun getAdData(): MutableMap<String, String> = mockk(relaxed = true)
       }
       testSpecificCase(fakeEvent)
+    }
+  }
+
+  @Test
+  fun testAdErrorsAlwaysForwarded() {
+    // This specific test doesn't care about what the SDK internals are doing at all
+    // We only want to make sure that the customer's ad listeners are always called
+    fun testSpecificCase(incomingError: AdErrorEvent) {
+      log(message = "Testing adError with type $incomingError")
+      val dummyExoPlayer = mockk<Player>(relaxed = true)
+      val dummyMuxStats = mockk<MuxStatsSdkMedia3<Player>>(relaxed = true) {
+        every { boundPlayer } returns dummyExoPlayer
+      }
+      val customerAdErrorListener = mockk<AdErrorListener> {
+        every { onAdError(any()) } just runs
+      }
+      val muxImaListener = MuxImaAdsListener.newListener(
+        dummyMuxStats,
+        mockk(relaxed = true), // error listener not under test
+        customerAdErrorListener
+      )
+
+      muxImaListener.onAdError(incomingError)
+
+      verify {
+        customerAdErrorListener.onAdError(
+          withArg { forwardedError ->
+            assertEquals(
+              "Error type should be ${incomingError.error.errorType}",
+              incomingError.error.errorType,
+              forwardedError.error.errorType
+            )
+            assertEquals(
+              "Error code should be ${incomingError.error.errorCode}",
+              incomingError.error.errorCode,
+              forwardedError.error.errorCode
+            )
+          }
+        )
+      } // verify
+    } // testSpecificCase
+
+    for (errType in AdErrorType.values()) {
+      for (errCode in AdErrorCode.values()) {
+        val fakeEvent = object : AdErrorEvent {
+          override fun getError(): AdError = AdError(errType, errCode, "fake error")
+          override fun getUserRequestContext(): Any = Object()
+          override fun toString() = "adError: $error"
+        }
+        testSpecificCase(fakeEvent)
+      }
     }
   }
 }
