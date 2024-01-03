26f132c8eec2f5bb2598d36d9e2853c70714444b
Emily Dixon <edixon@mux.com>
2024-01-03 09:58:41 -0800

    fix: populate ad data even for non-preroll ads

--- library-ima/src/main/java/com/mux/stats/sdk/media3_ima/MuxImaAdsListener.kt
+++ library-ima/src/main/java/com/mux/stats/sdk/media3_ima/MuxImaAdsListener.kt
@@ -59,17 +59,15 @@ class MuxImaAdsListener private constructor(
   private fun setupAdViewData(event: MuxAdEvent, ad: Ad?) {
     val viewData = ViewData()
     val adData = AdData();
-    if (adCollector?.playbackPositionMillis == 0L) {
-      if (ad != null) {
-        viewData.viewPrerollAdId = ad.adId
-        viewData.viewPrerollCreativeId = ad.creativeId
+    if (ad != null) {
+      viewData.viewPrerollAdId = ad.adId
+      viewData.viewPrerollCreativeId = ad.creativeId
 
-        exoPlayer?.getAdTagUrl()?.let { adData.adTagUrl = it }
-        ad.adId?.let { adData.adId = it }
-        ad.creativeId?.let { adData.adCreativeId = it }
-        @Suppress("DEPRECATION") // This is only deprecated on android, we need consistency
-        ad.universalAdIdValue?.let { adData.adUniversalId = it }
-      }
+      exoPlayer?.getAdTagUrl()?.let { adData.adTagUrl = it }
+      ad.adId?.let { adData.adId = it }
+      ad.creativeId?.let { adData.adCreativeId = it }
+      @Suppress("DEPRECATION") // This is only deprecated on android, we need consistency
+      ad.universalAdIdValue?.let { adData.adUniversalId = it }
     }
     event.viewData = viewData
     event.adData = adData;
