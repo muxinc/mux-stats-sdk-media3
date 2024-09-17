5d3d2db6ac250cfb8d40bb152dd1a3d218f6d020
Emily Dixon <edixon@mux.com>
2024-09-17 16:10:23 -0700

    more comment blablbah

--- app/src/compatFrom1_3/java/VersionCompat.kt
+++ app/src/compatFrom1_3/java/VersionCompat.kt
@@ -5,6 +5,9 @@ import androidx.media3.exoplayer.ima.ImaServerSideAdInsertionMediaSource
 /**
  * Functions with different Variants for different media3 versions. There are other version of this
  * file for other variants. See the sourceSet defs for more details
+ *
+ * Customers probably don't need a class like this. We have one because we support older versions of
+ * media3, so we need variants
  */
 object VersionCompat {
   @androidx.annotation.OptIn(UnstableApi::class)
