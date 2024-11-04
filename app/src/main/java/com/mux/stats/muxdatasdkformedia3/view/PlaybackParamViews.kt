2ec95b7e020c94452bcb9bc7bfef79b0aec43789
Emily Dixon <edixon@mux.com>
2024-11-04 10:57:47 -0800

    Here we go

--- app/src/main/java/com/mux/stats/muxdatasdkformedia3/view/PlaybackParamViews.kt
+++ app/src/main/java/com/mux/stats/muxdatasdkformedia3/view/PlaybackParamViews.kt
@@ -11,6 +11,59 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import com.mux.stats.muxdatasdkformedia3.R
 import com.mux.stats.muxdatasdkformedia3.databinding.NumericParamEntryBinding
 import com.mux.stats.muxdatasdkformedia3.databinding.ParamSpinnerWithDefaultBinding
+import com.mux.stats.muxdatasdkformedia3.databinding.TextParamEntryBinding
+
+class TextParamEntryView @JvmOverloads constructor(
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+  private val binding: TextParamEntryBinding = TextParamEntryBinding.inflate(
+    LayoutInflater.from(context),
+    this,
+    true
+  )
+
+  init {
+    context.theme.obtainStyledAttributes(attrs, R.styleable.TextParamEntryView, 0, 0).apply {
+      try {
+        hint = getString(R.styleable.TextParamEntryView_hint)
+      } finally {
+        recycle()
+      }
+    }
+    context.theme.obtainStyledAttributes(attrs, R.styleable.ParamEntry, 0, 0).apply {
+      try {
+        title = getString(R.styleable.ParamEntry_title)
+      } finally {
+        recycle()
+      }
+    }
+    binding.textParamEntryClear.setOnClickListener {
+      binding.textParamEntryIn.text = null
+      onClear?.invoke()
+    }
+  }
+
+  var title: CharSequence? = null
+    set(value) {
+      binding.textParamEntryLbl.text = value
+      field = value
+    }
+  var hint: CharSequence? = null
+    set(value) {
+      binding.textParamEntryIn.hint = value
+      field = value
+    }
+
+  var onClear: (() -> Unit)? = null
+  val entry: String?
+    get() {
+      val text = binding.textParamEntryIn.text?.trim()?.ifEmpty { null }?.toString()
+      return text
+    }
+}
 
 class SpinnerParamEntryView @JvmOverloads constructor(
   context: Context,
