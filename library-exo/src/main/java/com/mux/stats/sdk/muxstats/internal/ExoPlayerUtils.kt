432f299e806308154da3d8e2c5084fbb6eaf647d
Emily Dixon <edixon@mux.com>
2024-11-22 17:19:21 -0800

    better doc

--- library-exo/src/main/java/com/mux/stats/sdk/muxstats/internal/ExoPlayerUtils.kt
+++ library-exo/src/main/java/com/mux/stats/sdk/muxstats/internal/ExoPlayerUtils.kt
@@ -8,9 +8,8 @@ import androidx.media3.exoplayer.source.MediaSource
 /**
  * Returns true if the [MediaSource.MediaPeriodId] indicates that the period is in an ad break.
  *
- * Note that the default implementation for SSAI/DAI reports the input format late, doing so just
- * before switching off of the ad or content media. In this case, you can still use this method to
- * tell whether an input format change belongs to an ad period even if the timing is off.
+ * Note the the default IMA and Media3 implementation report changing back to the main content
+ * format before the ad break is over.
  */
 @OptIn(UnstableApi::class)
 fun MediaSource.MediaPeriodId.isInAdPeriod(): Boolean {
