3e22a366ee14a8540a37fcc4fe530c5dc07a8b4d
Emily Dixon <edixon@mux.com>
2025-07-01 15:46:45 -0700

    data type and track type debug strings

--- library-exo/src/main/java/com/mux/stats/sdk/muxstats/internal/ExoPlayerUtils.kt
+++ library-exo/src/main/java/com/mux/stats/sdk/muxstats/internal/ExoPlayerUtils.kt
@@ -15,3 +15,29 @@ import androidx.media3.exoplayer.source.MediaSource
 fun MediaSource.MediaPeriodId.isInAdGroup(): Boolean {
   return this.adGroupIndex != C.INDEX_UNSET || this.adIndexInAdGroup != C.INDEX_UNSET
 }
+
+fun trackTypeString(trackType: Int): String = when (trackType) {
+  C.TRACK_TYPE_NONE -> "none"
+  C.TRACK_TYPE_UNKNOWN -> "unknown"
+  C.TRACK_TYPE_DEFAULT -> "default"
+  C.TRACK_TYPE_AUDIO -> "audio"
+  C.TRACK_TYPE_VIDEO -> "video"
+  C.TRACK_TYPE_TEXT -> "text"
+  C.TRACK_TYPE_IMAGE -> "image"
+  C.TRACK_TYPE_METADATA -> "metadata"
+  C.TRACK_TYPE_CAMERA_MOTION -> "camera_motion"
+  else -> "other: $trackType"
+}
+
+@OptIn(UnstableApi::class)
+fun dataTypeString(dataType: Int): String = when (dataType) {
+  C.DATA_TYPE_UNKNOWN -> "unkown"
+  C.DATA_TYPE_MEDIA -> "media"
+  C.DATA_TYPE_MEDIA_INITIALIZATION -> "media_initialization"
+  C.DATA_TYPE_DRM -> "drm"
+  C.DATA_TYPE_MANIFEST -> "manifest"
+  C.DATA_TYPE_TIME_SYNCHRONIZATION -> "time_sycnhronization"
+  C.DATA_TYPE_AD -> "ad"
+  C.DATA_TYPE_MEDIA_PROGRESSIVE_LIVE -> "progressive_live"
+  else -> "other: $dataType"
+}
