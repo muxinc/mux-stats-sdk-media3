a03a4bd862993f01c83c13dd0fc171105a0882fb
Emily Dixon <edixon@mux.com>
2025-09-30 16:51:48 -0700

    update: Detect ad_type when using the IMA SDK

--- library-ima/src/main/java/com/mux/stats/sdk/media3_ima/MuxImaAdsListener.kt
+++ library-ima/src/main/java/com/mux/stats/sdk/media3_ima/MuxImaAdsListener.kt
@@ -1,5 +1,6 @@
 package com.mux.stats.sdk.media3_ima
 
+import android.util.Log
 import androidx.media3.common.Player
 import com.google.ads.interactivemedia.v3.api.Ad
 import com.google.ads.interactivemedia.v3.api.AdErrorEvent
@@ -12,6 +13,7 @@ import com.google.ads.interactivemedia.v3.api.player.VideoProgressUpdate
 import com.mux.android.util.oneOf
 import com.mux.stats.sdk.core.events.playback.*
 import com.mux.stats.sdk.core.model.AdData
+import com.mux.stats.sdk.core.model.AdType
 import com.mux.stats.sdk.core.model.ViewData
 import com.mux.stats.sdk.core.util.MuxLogger
 import com.mux.stats.sdk.muxstats.AdCollector
@@ -78,6 +80,18 @@ class MuxImaAdsListener private constructor(
       ad.creativeId?.let { adData.adCreativeId = it }
       @Suppress("DEPRECATION") // This is only deprecated on android, we need consistency
       ad.universalAdIdValue?.let { adData.adUniversalId = it }
+
+      val adType = ad.adPodInfo?.let {
+        // per the IMA docs
+        when (it.timeOffset) {
+          0.0 -> AdType.PRE_ROLL
+          -1.0 -> AdType.POST_ROLL
+          else -> AdType.MID_ROLL
+        }
+      }
+      adData.adType = adType
+      Log.d(TAG, "Set ad type to $adType")
+
     }
     event.viewData = viewData
     event.adData = adData
